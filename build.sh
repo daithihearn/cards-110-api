1ecbf2543b6c00f8cb25ae0f0eb7b5c21e3a95cb
Daithi Hearn <daithi.hearn@gmail.com>
2020-05-28 19:11:23 +0200

    Improving build scripts

--- build.sh
+++ build.sh
@@ -5,7 +5,11 @@ echo "
 
 ./gradlew build install
 
-echo "
-2. Building image cards-110-api"
-
-docker build -t localhost:5000/cards-110-api:latest .
+if [ -x "$(command -v docker)" ]; then
+	echo "
+	2. Building image cards-110-api"
+	docker build -t localhost:5000/cards-110-api:latest .
+	docker push localhost:5000/cards-110-api:latest
+else
+    echo "Docker not installed"
+fi
