bd54b1a0f128dd35de83523efe599a1819df68ce
Daithi Hearn <daithi.hearn@gmail.com>
2020-11-07 21:38:37 +0100

    Fixing windows build script

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 
 ECHO "1. Building cards-110-api"
 
-gradlew.bat build install
+CALL gradlew.bat build install
 
 ECHO "2. Building image cards-110-api"
 CALL docker build -t localhost:5000/cards-110-api:latest .
