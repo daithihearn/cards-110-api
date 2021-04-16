e718687aa36d0e730ef87746a52eef3354af228e
Daithi Hearn <daithi.hearn@gmail.com>
2021-04-16 04:35:53 +0200

    Simplifying build scripts

--- build.sh
+++ build.sh
@@ -1,18 +1,10 @@
 #!/bin/sh
 
-CARDS_API_VERSION=$(sed -n 's/^CARDS_API_VERSION=//p' .env)
-
-echo "
-1. Building jar cards-110-api"
-
-./gradlew build install
-
 if [ -x "$(command -v docker)" ]; then
 	echo "
-	2. Building image cards-110-api"
-	docker build --build-arg VERSION=${CARDS_API_VERSION} -t localhost:5000/cards-110-api:latest -t localhost:5000/cards-110-api:${CARDS_API_VERSION} .
+	Building image cards-110-api"
+	docker build -t localhost:5000/cards-110-api:latest .
 	docker push localhost:5000/cards-110-api:latest
-	docker push localhost:5000/cards-110-api:${CARDS_API_VERSION}
 else
     echo "Docker not installed"
 fi
