8993407de43c86be8676a83523ba876a33fb245c
dahearn <dahearn@deloitte.ie>
2021-05-08 01:55:32 +0200

    Adding game stats

---
+++ src/main/kotlin/ie/daithi/cards/service/StatsService.kt
@@ -0,0 +1,29 @@
+package ie.daithi.cards.service
+
+import ie.daithi.cards.enumeration.GameStatus
+import ie.daithi.cards.model.Game
+import ie.daithi.cards.model.PlayerGameStats
+import org.springframework.data.mongodb.core.MongoOperations
+import org.springframework.data.mongodb.core.aggregation.Aggregation
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.stereotype.Service
+
+@Service
+class StatsService(
+    private val mongoOperations: MongoOperations
+    ) {
+
+    fun gameStatsForPlayer(playerId: String): List<PlayerGameStats> {
+        val match1 = Aggregation.match(Criteria.where("status").`is`(GameStatus.FINISHED).and("players._id").`is`(playerId))
+        val unwind = Aggregation.unwind("\$players")
+        val match2 = Aggregation.match(Criteria.where("players._id").`is`(playerId))
+        val project = Aggregation.project()
+            .and("\$id").`as`("gameId")
+            .and("\$players.winner").`as`("winner")
+            .and("\$players.score").`as`("score")
+            .and("\$players.rings").`as`("rings")
+
+        val aggregation = Aggregation.newAggregation(match1, unwind, match2, project)
+        return mongoOperations.aggregate(aggregation, Game::class.java, PlayerGameStats::class.java).mappedResults
+    }
+}
---
+++ src/main/kotlin/ie/daithi/cards/web/controller/StatsController.kt
@@ -0,0 +1,37 @@
+package ie.daithi.cards.web.controller
+
+import ie.daithi.cards.model.PlayerGameStats
+import ie.daithi.cards.service.AppUserService
+import ie.daithi.cards.web.exceptions.ForbiddenException
+import ie.daithi.cards.service.StatsService
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiResponse
+import io.swagger.annotations.ApiResponses
+import org.springframework.http.HttpStatus
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/v1")
+@Api(tags = ["Statistics"], description = "Endpoints that relate to game statistics")
+class StatsController(
+    private val appUserService: AppUserService,
+    private val statsService: StatsService
+) {
+
+    @GetMapping("/stats/gameStatsForPlayer")
+    @ResponseStatus(value = HttpStatus.OK)
+    @ApiOperation(value = "Game stats for current player", notes = "Returns the game stats for the current player")
+    @ApiResponses(
+            ApiResponse(code = 200, message = "Request successful")
+    )
+    @ResponseBody
+    fun gameStatsForPlayer(): List<PlayerGameStats> {
+        // 1. Get current user ID
+        val subject = SecurityContextHolder.getContext().authentication.name ?: throw ForbiddenException("Couldn't authenticate user")
+        val user = appUserService.getUserBySubject(subject)
+
+        return statsService.gameStatsForPlayer(user.id!!)
+    }
+}
