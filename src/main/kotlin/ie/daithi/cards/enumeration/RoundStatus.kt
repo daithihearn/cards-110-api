6c413dd2ce18aab2bf7a90c9505fda82316ebb7d
Daithi Hearn <daithi.hearn@gmail.com>
2023-01-14 17:04:00 +0100

    Fixing auto deal Wrong status was being set

--- src/main/kotlin/ie/daithi/cards/enumeration/RoundStatus.kt
+++ src/main/kotlin/ie/daithi/cards/enumeration/RoundStatus.kt
@@ -1,7 +1,8 @@
 package ie.daithi.cards.enumeration
 
-enum class RoundStatus(
-        val order: Int
-) {
-    CALLING(0), CALLED(1), BUYING(2), PLAYING(3);
+enum class RoundStatus(val order: Int) {
+    CALLING(0),
+    CALLED(1),
+    BUYING(2),
+    PLAYING(3)
 }
--- src/main/kotlin/ie/daithi/cards/service/StatsService.kt
+++ src/main/kotlin/ie/daithi/cards/service/StatsService.kt
@@ -9,22 +9,33 @@ import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.stereotype.Service
 
 @Service
-class StatsService(
-    private val mongoOperations: MongoOperations
-    ) {
+class StatsService(private val mongoOperations: MongoOperations) {
 
     fun gameStatsForPlayer(playerId: String): List<PlayerGameStats> {
-        val match1 = Aggregation.match(Criteria.where("status").`is`(GameStatus.FINISHED).and("players._id").`is`(playerId))
+        val match1 =
+                Aggregation.match(
+                        Criteria.where("status")
+                                .`is`(GameStatus.FINISHED)
+                                .and("players._id")
+                                .`is`(playerId)
+                )
         val unwind = Aggregation.unwind("\$players")
         val match2 = Aggregation.match(Criteria.where("players._id").`is`(playerId))
-        val project = Aggregation.project()
-            .and("\$id").`as`("gameId")
-            .and("\$timestamp").`as`("timestamp")
-            .and("\$players.winner").`as`("winner")
-            .and("\$players.score").`as`("score")
-            .and("\$players.rings").`as`("rings")
+        val project =
+                Aggregation.project()
+                        .and("\$id")
+                        .`as`("gameId")
+                        .and("\$timestamp")
+                        .`as`("timestamp")
+                        .and("\$players.winner")
+                        .`as`("winner")
+                        .and("\$players.score")
+                        .`as`("score")
+                        .and("\$players.rings")
+                        .`as`("rings")
 
         val aggregation = Aggregation.newAggregation(match1, unwind, match2, project)
-        return mongoOperations.aggregate(aggregation, Game::class.java, PlayerGameStats::class.java).mappedResults
+        return mongoOperations.aggregate(aggregation, Game::class.java, PlayerGameStats::class.java)
+                .mappedResults
     }
 }
