13ac30531a51af05b07e13f4da318b900d16effe
Daithi Hearn <daithi.hearn@gmail.com>
2023-05-17 13:29:00 +0200

    Fixing swagger

---
+++ src/main/kotlin/ie/daithi/cards/config/OpenApiConfig.kt
@@ -0,0 +1,20 @@
+package ie.daithi.cards.config
+
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.servers.Server
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class OpenApiConfig {
+
+    @Bean
+    fun openAPI(@Value("\${api.base.path:'/'}") customServerUrl: String,
+                @Value("\${app.version}") appVersion: String): OpenAPI = OpenAPI()
+        .info(Info().title("Cards 110 API").version(appVersion).description("The API for the Cards 110 game"))
+        .addServersItem(Server().url(customServerUrl).description("Cards API"))
+        .addSecurityItem(SecurityRequirement().addList("bearerAuth", emptyList()))
+}
