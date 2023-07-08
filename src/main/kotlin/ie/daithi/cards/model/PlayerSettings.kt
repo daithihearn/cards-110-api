10e642d9aeff63504ead0ab2d6cbaa0f9fda523d
Daithi Hearn <daithi.hearn@gmail.com>
2023-07-08 11:12:03 +0200

    Fixing typo

--- src/main/kotlin/ie/daithi/cards/model/PlayerSettings.kt
+++ src/main/kotlin/ie/daithi/cards/model/PlayerSettings.kt
@@ -1,7 +1,7 @@
 package ie.daithi.cards.model
 
 import com.fasterxml.jackson.annotation.JsonIgnore
-import org.springframework.data.annotation.Id~
+import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document(collection = "playerSettings")
